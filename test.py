8537192582040da5f7a4a0d5570303124c7ee00f
BOUR.KLAN <BOUR.KLAN@PNC.LAN>
2023-08-01 08:47:58 +0700

    test

---
+++ test.py
@@ -0,0 +1,16 @@
+# print(type("145"))
+# print(type(145))
+# print(type(True))
+# print(type(145.5))
+
+# number = "string"
+# result = float(number)
+# print(result)
+
+number = "3"
+number = int(number)
+for i in range(number):
+    if number < 3:
+        print("Small")
+    else:
+        print("Greater")
